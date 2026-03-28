b5bac68fd36d49c34327635f17e89a7f029297d6
Sauzan Faza <sauzanfaza@gmail.com>
2026-03-28 10:08:55 +0700

    learning abot two way binding with v-model

---
+++ src/components/VModel.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('')
+</script>
+
+<template>
+  <!-- tanpa v-model -->
+  <input :value="text" @input="(e) => (text = e.target.value)" />
+
+  <!-- dengan v-model -->
+  <input v-model="text" />
+</template>
