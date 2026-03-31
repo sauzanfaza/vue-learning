510e901d44f67ca8e44af0e1160482e8895fbb73
Sauzan Faza <sauzanfaza@gmail.com>
2026-03-31 09:12:25 +0700

    learning onMounted() and watch()

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,46 @@
 import Navbar from './components/Navbar.vue'
 import InputMhs from './components/InputMhs.vue'
 import RenderingData from './components/RenderingData.vue'
+import SearchBar from './components/SearchBar.vue'
 
-import { ref } from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
+
+const search = ref('')
 
 const data = ref([
   { id: 1, name: 'Susan', semester: 7 },
-  { id: 2, name: 'Puput', semester: 7 },
-  { id: 3, name: 'Nuy', semester: 7 },
-  { id: 4, name: 'Aura', semester: 7 },
+  { id: 2, name: 'Chanyeol', semester: 7 },
+  { id: 3, name: 'Kai', semester: 7 },
+  { id: 4, name: 'Sehun', semester: 7 },
 ])
+
+// onMounted(() => {
+//   data.value = [
+//     { id: 1, name: 'Susan', semester: 7 },
+//     { id: 2, name: 'Chanyeol', semester: 7 },
+//     { id: 3, name: 'Kai', semester: 7 },
+//     { id: 4, name: 'Sehun', semester: 7 },
+//   ]
+// })
+// onMounted itu mirip useEffect() tapi klo onMouonted cuman sekali doang, dia dipakai buat jalan sekali saat component muncul
+// biasanya buat fetch API
+// onMounted(() => {
+//   fetchData()
+// }, [])
+
+//onMounted simpelnya = pas pertama kali buka halaman, jalanin ini
+
+// perlu diingat sannn!!!
+// ref --> buat simpen data
+//onMounted --> buat waktu jalanin logic
+// computed --> buat olah data
+
+// watch = useEffect() alias ngereaksiin perubahan
+// bentuk dasar: watch(apa_yang_dipantau, callback)
+
+const filteredData = computed(() => {
+  return data.value.filter((mhs) => mhs.name.toLowerCase().includes(search.value.toLowerCase()))
+})
 
 function tambahMhs(mhsBaru) {
   const lastId = data.value.length > 0 ? data.value[data.value.length - 1].id : 0
@@ -26,12 +57,21 @@ function tambahMhs(mhsBaru) {
     ...mhsBaru,
   })
 }
+
+function handleSearch(val) {
+  search.value = val
+  watch(search, (val) => {
+    console.log('susan lagi search: ', val) //liat di console tiap huruf yang di ketik di search bar di watch sama fungsi watch wkwk
+  })
+  //watch ini buat jalanin efek tambahan biasanya, dan dia gak return value
+}
 </script>
 
 <template>
   <Navbar />
   <InputMhs @add-mhs="tambahMhs" />
-  <RenderingData :data="data" />
+  <SearchBar @update-search="handleSearch" />
+  <RenderingData :data="filteredData" />
   <!-- <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,53 @@
+<!-- Mempelajari computed dengan filter di searchbar -->
+<template>
+  <div class="flex">
+    <input
+      type="text"
+      placeholder="input text"
+      v-model="search"
+      @input="searchBar"
+      class="flex border m-4 py-2 px-4 rounded-2xl"
+    />
+    <button
+      @click="searchBar"
+      class="bg-green-500 py-2 px-4 m-4 rounded-2xl hover:bg-green-600 cursor-pointer"
+    >
+      search
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const search = ref('')
+const emit = defineEmits(['update-search'])
+
+function searchBar() {
+  emit('update-search', search.value)
+}
+</script>
+
+<style lang="scss" scoped></style>
+
+<!-- SearchBar (emit keyword)
+        ↓
+App.vue (simpan search)
+        ↓
+computed (filter data)
+        ↓
+RenderingData (tampil hasil) -->
+
+<!-- computed: data turunan otomatis dari state utama
+ gampangnya: data yang dihitung dari data lain, dan bakal 
+ update sendiri kalo sumbernya berubah
+ 
+ Flownya:
+ data berubah
+atau
+search berubah
+        ↓
+computed jalan otomatis
+        ↓
+UI ikut update
+-->
